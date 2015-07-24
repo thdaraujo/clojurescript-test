4395f86339249a5bbdf8e92e18edcdf85902ce45
Thiago Araujo <worf.mogh@gmail.com>
2015-07-24 10:30:03 -0300

    project.clj

--- project.clj
+++ project.clj
@@ -3,13 +3,29 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [org.clojure/tools.reader "0.8.4"]
                  [compojure "1.3.2"]
                  [ring/ring-core "1.3.2"]
                  [ring/ring-json "0.3.1"]
-                 [ring/ring-defaults "0.1.4"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [cljs-http "0.1.21"]
+                 [om "0.7.3"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+           [lein-ring "0.8.13"]
+           [lein-pdo "0.1.1"]]
+  :aliases {"up" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
+  :cljsbuild {:builds [{:id "dev"
+                      :source-paths ["src/cljs"]
+                      :compiler {:output-to "resources/public/js/app.js"
+                                 :output-dir "resources/public/js/out"
+                                 :optimizations :none
+                                 :source-map true}}]}
   :ring {:handler acme.handler/app}
+  :source-paths ["src/clj"]
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
+
+
 
